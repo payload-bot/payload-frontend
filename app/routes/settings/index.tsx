1f3d3503f16404c7bb7192cff05b61fbb57269bc
c43721 <rlahman.game@gmail.com>
2022-01-02 13:10:43 -0600

    feat: style settings page

--- app/routes/settings/index.tsx
+++ app/routes/settings/index.tsx
@@ -3,45 +3,61 @@ import { ActionFunction, json, useLoaderData } from "remix";
 import { useUser } from "../settings";
 
 export const action: ActionFunction = async ({ request }) => {
-  const data = await request.formData();
+  const form = await request.formData();
 
-  // do something with form, like validations!
+  const steamId = form.get("steamId");
+
+  return { success: true, errors: {} };
 };
 
 export default function User() {
   const user = useUser();
-  const { message } = useLoaderData<{ message: string }>();
 
   return (
-    <div className="mt-8 max-w-5xl mx-auto">
-      {message ? message : null}
-      <h1 className="font-medium text-gray-800 text-2xl">Profile Settings</h1>
-      <hr className="divide-x-2  my-4" />
-      <h1 className="font-medium text-gray-800 text-2xl">General</h1>
-      <form action="settings?index" method="post">
-        <input
-          type="text"
-          placeholder="Your SteamID"
-          defaultValue={user.steamId ?? ""}
-        />
-        <input type="range" min="0" max="3" />
-        <button className="py-2 px-4 rounded-md bg-transparent hover:border-sky-600 hover:bg-sky-200 border-2 text-sm text-sky-600 border-sky-500 uppercase font-bold transition duration-150">
-          Save
-        </button>
-      </form>
+    <div className="mt-8 max-w-5xl mx-auto px-8">
+      <h1 className="font-bold text-gray-800 text-2xl">Profile Settings</h1>
 
-      <hr className="divide-x-2  my-4" />
-      <h1 className="font-medium text-gray-800 text-2xl">Quick Actions</h1>
-      <div className="flex flex-row flex-wrap gap-4">
-        <form action="/logout" method="post">
-          <button className="py-2 px-4 rounded-md  hover:bg-red-300 hover:text-red-700 bg-red-200 text-red-600 font-semibold transition duration-100">
+      <hr className="divide-x-2 my-6 sm:my-4" />
+
+      <div className="grid sm:grid-cols-2">
+        <div className="space-y-4 sm:space-y-2 mb-4 sm:mb-0">
+          <h1 className="font-semibold text-gray-800 text-2xl">General</h1>
+          <p className="text-md text-gray-500 font-medium w-full sm:w-3/4">
+            General account settings. Please note this information is public!
+          </p>
+        </div>
+        <form
+          action="settings?index"
+          method="post"
+          className="flex flex-col gap-2"
+        >
+          <label htmlFor="steamId" className="text-gray-700 font-medium">
+            SteamID
+          </label>
+          <input
+            className="rounded-lg focus:border-sky-500"
+            type="text"
+            id="steamId"
+            placeholder="Your SteamID"
+            defaultValue={user.steamId ?? ""}
+          />
+          <button className="w-full mt-2 sm:mt-0 sm:w-max text-md sm:text-sm  py-2 px-4 rounded-md bg-sky-600 hover:bg-sky-700 border text-white border-sky-700 uppercase font-bold transition duration-150">
+            Save
+          </button>
+        </form>
+      </div>
+
+      <hr className="divide-x-2 my-6 sm:my-4" />
+      <h1 className="font-medium text-gray-800 text-2xl mb-2">Quick Actions</h1>
+      <div className="flex flex-row-reverse sm:flex-row flex-wrap gap-2 sm:gap-4 w-full">
+        <button className="py-2 px-4 rounded-md w-full sm:w-max hover:bg-red-300 bg-red-200 border border-red-600 text-red-600 font-semibold transition duration-100">
+          Delete User Data
+        </button>
+        <form action="/logout" method="post" className="w-full sm:w-max">
+          <button className="py-2 px-4 rounded-md w-full sm:w-max hover:bg-red-100 bg-transparent border border-red-600 text-red-600 font-semibold transition duration-100">
             Logout
           </button>
         </form>
-
-        <button className="py-2 px-4 rounded-md hover:bg-red-300 hover:text-red-700 bg-red-200 text-red-600 font-semibold transition duration-100">
-          Delete User Data
-        </button>
       </div>
     </div>
   );
