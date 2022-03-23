018637cb8ef908cfbc6d7b224df6cd810db05098
c43721 <rlahman.game@gmail.com>
2022-03-23 16:22:50 -0500

    fix(*): lint

--- app/routes/privacy.tsx
+++ app/routes/privacy.tsx
@@ -2,7 +2,7 @@ import Component from "~/content/legal/privacy.mdx";
 
 export default function Privacy() {
   return (
-    <div className="mt-6 pb-10 mx-auto prose dark:prose-invert max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-4xl">
+    <div className="prose mx-auto mt-6 max-w-xs pb-10 dark:prose-invert sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-4xl">
       <Component />
     </div>
   );
