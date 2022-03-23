ee12237a48a2e5a0b0ffcd6c3a2ebab133030a48
c43721 <rlahman.game@gmail.com>
2022-03-23 18:50:39 -0500

    feat: animate in the popover

--- app/routes/dashboard/$id/commands.tsx
+++ app/routes/dashboard/$id/commands.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { Transition } from "@headlessui/react";
+import { Fragment, useEffect, useState } from "react";
 import { ActionFunction, Form, json, useTransition } from "remix";
 import CommandToggle from "~/components/CommandToggle";
 import { makeApiRequest } from "~/utils/api.server";
@@ -85,24 +86,38 @@ export default function Commands() {
           />
         ))}
 
-      {saving ? (
-        <div className="fixed inset-x-0 bottom-4 mx-auto flex max-w-screen-md items-center rounded-lg bg-black/90 px-2 py-4">
-          <p className="text-md flex-1 font-medium text-gray-300 dark:text-white">
-            Please save your changes!
-          </p>
-          {/* I think I need to get a better version of this <button className="text-md mr-4 font-medium text-white">Reset</button> */}
-          <Form replace method="post" className="flex gap-4">
-            <input type="hidden" name="commands" value={commandsToRestrict} />
-            <button
-              className="text-md rounded-md bg-green-400 py-1 px-3 font-medium text-green-800 transition duration-150 hover:bg-green-500 disabled:bg-green-500/30 dark:bg-green-500 dark:text-green-900 dark:hover:bg-green-600"
-              disabled={submitting}
-              type="submit"
-            >
-              {submitting ? "Saving your changes..." : "Save Changes"}
-            </button>
-          </Form>
-        </div>
-      ) : null}
+      <Transition
+        as={Fragment}
+        show={saving}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-6"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-200"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-6"
+      >
+        {saving ? (
+          <div className="fixed inset-x-0 bottom-4 mx-auto flex max-w-screen-md items-center rounded-lg bg-black/90 px-2 py-4">
+            <p className="text-md flex-1 font-medium text-gray-300 dark:text-white">
+              Please save your changes!
+            </p>
+            {/* I think I need to get a better version of this <button className="text-md mr-4 font-medium text-white">Reset</button> */}
+            <Form replace method="post" className="flex gap-4">
+              <input type="hidden" name="commands" value={commandsToRestrict} />
+              <button
+                className="text-md rounded-md bg-green-400 py-1 px-3 font-medium text-green-800 transition duration-150 hover:bg-green-500 disabled:bg-green-500/30 dark:bg-green-500 dark:text-green-900 dark:hover:bg-green-600"
+                disabled={submitting}
+                type="submit"
+              >
+                {submitting ? "Saving your changes..." : "Save Changes"}
+              </button>
+            </Form>
+          </div>
+        ) : (
+          // HACK
+          <></>
+        )}
+      </Transition>
     </section>
   );
 }
