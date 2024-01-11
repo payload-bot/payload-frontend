e28d0b5c429e7f9b2367b2a133cc3fce6789237a
c43721 <rlahman.game@gmail.com>
2024-01-11 17:34:48 -0600

    chore: update v2, use client loader

--- app/routes/discord.tsx
+++
@@ -1,5 +0,0 @@
-import { LoaderFunction, redirect } from "@remix-run/node";
-
-export const loader: LoaderFunction = () => {
-  return redirect("https://discord.com/invite/gYnnMYz", { status: 301 });
-};
