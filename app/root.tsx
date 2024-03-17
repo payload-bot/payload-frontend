5523dcec50d23aa0bd5779e84a6140580e331091
c43721 <rlahman.game@gmail.com>
2024-03-17 16:47:33 -0500

    fix: fix it all

--- app/root.tsx
+++ app/root.tsx
@@ -43,21 +43,26 @@ export function links() {
   ];
 }
 
-export default function Root() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body className="dark:bg-gray-800">
-        <Outlet />
+        {children}
         <ScrollRestoration />
+        <Scripts />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Outlet />;
 }
 
 export function ErrorBoundary() {
